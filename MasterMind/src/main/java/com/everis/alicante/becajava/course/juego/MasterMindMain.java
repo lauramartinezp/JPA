045f0ac4a96e36ba7020dd1fb116530dcc7bdc98
Laura <lauramartinezp@outlook.es>
2017-07-27 20:11:01 +0200

    juego

--- MasterMind/src/main/java/com/everis/alicante/becajava/course/juego/MasterMindMain.java
+++ MasterMind/src/main/java/com/everis/alicante/becajava/course/juego/MasterMindMain.java
@@ -2,35 +2,82 @@ package com.everis.alicante.becajava.course.juego;
 
 import java.util.Scanner;
 
+import mingorance.enrique.commandlinemenu.Menu;
+import mingorance.enrique.commandlinemenu.MenuBuilder;
+
 public class MasterMindMain {
 
 	public static void main(String[] args) {
+		
+		
 
-		Scanner in = new Scanner(System.in);
+        Menu subMenu1 = MenuBuilder.createSubMenu("Menu Secundario 1", 1)
+                .addSimpleOption(1, "Offline")
+                .addSimpleOption(2, "Servidor local")
+                .addSimpleOption(3, "Online")
+                .addSimpleOption(4, "Nain")
+                .addExitOption(0, "SalirS1")
+                .build();
 
-		Integer numeroInicial = (int) (Math.random() * 100000);
+        Menu subMenu2 = MenuBuilder.createSubMenu("Menu Secundario 2", 2)
+                .addSimpleOption(1, "Opcion 1")
+                .addSimpleOption(2, "OpciónS2 2")
+                .addSimpleOption(3, "OpciónS2 3")
+                .addSimpleOption(4, "OpciónS2 4")
+                .addExitOption(0, "SalirS2")
+                .build();
 
-		MasterMindManager manager = new MasterMindManager(numeroInicial);
 
-		System.out.println("Bienvenidos jugadores, se crear� un numero aleatorio para poder jugar.");
+        Menu mainMenu = MenuBuilder.createMainMenu("Menu Principal")
+                .addSubMenuOption("Jugar MasterMind", subMenu1)
+                .addSubMenuOption("Submenú 2", subMenu2)
+                .addExitOption(0, "Salir")
+                .build();
 
-		ConsultaNumeroResult consultaNumeroResult;
+        switch(mainMenu.play()) {
+        case "1.1":
+        	iniciarAplicacionOffline();
+        	break;
+        }
+        
 
-		do {
-
-			System.out.println("Inserte un numero del 00000 al 99999");
-			Integer numero = in.nextInt();
-			consultaNumeroResult = manager.consultaNumero(numero);
-			System.out.println(consultaNumeroResult);
-
-		} while (manager.tieneIntentos() && !manager.getWin(consultaNumeroResult));
-		if (manager.getWin(consultaNumeroResult)) {
-			System.out.println("Ha ganado");
-
-		} else {
-			System.out.println("Has perdido");
-		}
-
+		
+		
+		
+		
 	}
 
+
+
+public static void iniciarAplicacionOffline() {
+
+	Scanner in = new Scanner(System.in);
+
+	Integer numeroInicial = (int) (Math.random() * 100000);
+
+	MasterMindManager manager = new MasterMindManager(numeroInicial);
+
+	System.out.println("Bienvenidos jugadores, se crear� un numero aleatorio para poder jugar.");
+
+	ConsultaNumeroResult consultaNumeroResult;
+
+	do {
+
+		System.out.println("Inserte un numero del 00000 al 99999");
+		Integer numero = in.nextInt();
+		consultaNumeroResult = manager.consultaNumero(numero);
+		System.out.println(consultaNumeroResult);
+
+	} while (manager.tieneIntentos() && !manager.getWin(consultaNumeroResult));
+	if (manager.getWin(consultaNumeroResult)) {
+		System.out.println("Ha ganado");
+
+	} else {
+		System.out.println("Has perdido");
+	}
+
+	
+	
 }
+}
+
